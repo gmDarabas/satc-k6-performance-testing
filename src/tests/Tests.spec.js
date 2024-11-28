7c6b0a8ffc125408822d1d780fe73ea95d9359ae
gmDarabas <gmd.darabas@hotmail.com>
2024-11-28 19:48:18 -0300

    testes funcionando

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -6,16 +6,33 @@ import { Trend, Rate } from 'k6/metrics';
 
 export const getContactsDuration = new Trend('get_contacts', true);
 export const RateContentOK = new Rate('content_OK');
+export const getDurationTrend = new Trend('get_duration')
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_duration: ['p(95)<5700'],
+    http_req_failed: ['rate<0.12'],
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
+    { duration: '10s', target: 10 },
+    { duration: '10s', target: 15 },
+    { duration: '40s', target: 20 },
+
+    { duration: '10s', target: 40 },
+    { duration: '10s', target: 50 },
+    { duration: '40s', target: 60 },
+
+    { duration: '10s', target: 75 },
+    { duration: '10s', target: 90 },
+    { duration: '40s', target: 105 },
+
+    { duration: '10s', target: 130 },
+    { duration: '10s', target: 155 },
+    { duration: '40s', target: 180 },
+
+    { duration: '10s', target: 230 },
+    { duration: '10s', target: 280 },
+    { duration: '40s', target: 300 },
   ]
 };
 
@@ -27,20 +44,16 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://portal-homolog.engeplus.com.br/';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  const params = {};
 
   const OK = 200;
 
   const res = http.get(`${baseUrl}`, params);
 
   getContactsDuration.add(res.timings.duration);
-
+  getDurationTrend.add(res.timings.duration);
   RateContentOK.add(res.status === OK);
 
   check(res, {
